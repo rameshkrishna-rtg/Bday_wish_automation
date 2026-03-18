923451598e77da999e8e908e7c7f1852cf24047b
rameshkrishna-rtg <rameshkrishnartg@gmail.com>
2026-03-18 17:51:44 +0530

    Sending one email request

---
+++ Gmail.js
@@ -0,0 +1,26 @@
+const { error } = require("node:console");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+const mailOptions = {
+    from: "rameshkrishnartg@gamil.com",
+    to: "iamkrish423@gmail.com",
+    subject: "Hello from rtg",
+    text: "This is a test mail"   
+};
+
+transporter.sendMail(mailOptions, (error,info)=> {
+    if(error){
+        console.log("Error sending email: ",error);
+    } else {
+        console.log("Email sent successfully!", info.response)
+    }
+})
