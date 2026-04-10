947e59c35d9ceff63490a7dabef379fb673ce678
rameshkrishna-rtg <rameshkrishnartg@gmail.com>
2026-04-10 12:35:37 +0530

    Send birthday wish API created

--- src/controllers/gmailController.js
+++ src/controllers/gmailController.js
@@ -1,4 +1,4 @@
-
+const cron = require("node-cron");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const db = require("../configs/dbConfig");
@@ -10,6 +10,56 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS,
     },
 });
+
+const sendBirthdayWishes = async (req, res) => {
+    console.log("Birthday cron started", new Date().toISOString());
+
+    //Get today's month and day
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const day = today.getDay();
+
+    try {
+        //Query today's birthdays and remove ther duplicates
+        const birthdays = await db.friends.findMany({
+            where: {
+                AND: [
+                    { birthMonth: month },
+                    { birthDay: day },
+                    {
+                        //Reject friends who got already received message
+                        emailLogs: {
+                            none: {
+                                status: "SUCCESS",
+                                createdAt: {
+                                    gte: new Date(today.setHours(0, 0, 0, 0)),
+                                    lt: new Date(today.setHours(23, 59, 59, 999))
+                                }
+
+                            }
+                        }
+                    }
+                ]
+            }
+        });
+
+        if(birthdays.length === 0){
+            console.log("No birthdays today");
+            return res.status(200).json({
+                status: "success",
+                message: "No birthdays today"
+            })
+        }
+
+        console.log("Birthdays found: ", birthdays.length);
+
+
+
+
+    } catch (error) {
+
+    }
+}
 
 const sendMail = async (req, res) => {
     try {
@@ -60,7 +110,7 @@ const sendMail = async (req, res) => {
                 status: "success",
                 message: "Email sent successfully!"
             })
-                
+
         } catch (error) {
             console.log("Email not sent", error.message)
 
