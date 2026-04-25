8ad38c41ab11d18a73d69ebaad0c8f73effc23c5
Ramesh krishna R <rameshkrishnartg@gmail.com>
2026-04-25 18:41:53 +0530

    Update gmailRoutes.js

--- src/routes/gmailRoutes.js
+++ src/routes/gmailRoutes.js
@@ -1,9 +1,4 @@
-const express = require("express");
-const route = express.Router();
-const multer = require("multer");
-const upload = multer();
 
-const { sendBirthdayWishes } = require("../controllers/gmailController")
 
 route.post("/email", upload.single("attachments"), sendBirthdayWishes)
 
